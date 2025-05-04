8433969a872a19e36c3407d4ebbd02e1e07166d6
kibrus <kibrussmenore@gmai.com>
2025-05-04 01:29:17 -0500

    final

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-﻿/*****************************************************************************
- * Challenge 2: Review the provided code. The provided code includes:
- * -> Statements that import data from games.js
- * -> A function that deletes all child elements from a parent element in the DOM
-*/
-
-// import the JSON data about the crowd funded games from the games.js file
+﻿// import the JSON data about the crowd funded games from the games.js file
 import GAMES_DATA from './games.js';
 
 // create a list of objects to store the data about the games using JSON.parse
@@ -16,11 +10,6 @@ function deleteChildElements(parent) {
         parent.removeChild(parent.firstChild);
     }
 }
-
-/*****************************************************************************
- * Challenge 3: Add data about each game as a card to the games-container
- * Skills used: DOM manipulation, for loops, template literals, functions
-*/
 
 // grab the element with the id games-container
 const gamesContainer = document.getElementById("games-container");
@@ -51,10 +40,6 @@ function addGamesToPage(games) {
 }
 
 addGamesToPage(GAMES_JSON);
-
-/*************************************************************************
- * Challenge 4: Create the summary statistics at the top of the page
- ************************************************************************/
 
 // Step 1: Total individual contributions
 const contributionsCard = document.getElementById("num-contributions");
@@ -105,15 +90,6 @@ unfundedBtn.addEventListener("click", filterUnfundedOnly);
 fundedBtn.addEventListener("click", filterFundedOnly);
 allBtn.addEventListener("click", showAllGames);
 
-
-
-/*************************************************************************************
- * Challenge 6: Add more information at the top of the page about the company.
- * Skills used: template literals, ternary operator
-*/
-/*****************************************************************************
- * Challenge 6: Add more information at the top of the page about the company
- *****************************************************************************/
 
 // grab the description container
 const descriptionContainer = document.getElementById("description-container");
